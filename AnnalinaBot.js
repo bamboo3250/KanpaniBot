f704bd28c0d4d91a6eb2fa6e5db34f912b97dce6
Truong Ngoc Khanh <bambooVN_3250@yahoo.com>
2016-10-24 22:51:02 +0800

    refactor answer of annalina

--- AnnalinaBot.js
+++ AnnalinaBot.js
@@ -29,28 +29,7 @@ function getCurrentTime() {
     return now.getTime();
 }
 
-var answerTexts = [
-    "It is certain",
-    "It is decidedly so",
-    "Without a doubt",
-    "Yes, definitely",
-    "You may rely on it",
-    "As I see it, yes",
-    "Most likely",
-    "Outlook good",
-    "Yes",
-    "Signs point to yes",
-    "Reply hazy try again",
-    "Ask again later",
-    "Better not tell you now",
-    "Cannot predict now",
-    "Concentrate and ask again",
-    "Don't count on it",
-    "My reply is no",
-    "My sources say no",
-    "Outlook not so good",
-    "Very doubtful"
-];
+var answerTexts = dialog.annalina.answers;
 
 function handleQuestion(message) {
     message.reply(answerTexts[randomInt(answerTexts.length)]);
