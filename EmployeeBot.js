1776b79c4692ccee3a9db39455750d2e6f365792
Truong Ngoc Khanh <bambooVN_3250@yahoo.com>
2016-11-17 12:46:19 +0800

    add ~mytop command

--- EmployeeBot.js
+++ EmployeeBot.js
@@ -803,6 +803,64 @@ EmployeeBot.prototype.handleTopCommand = function(message) {
     });
 }
 
+EmployeeBot.prototype.handleMyTopCommand = function(message) {
+    var text = message.content.trim().toLowerCase();
+    if (text != "~mytop") return;
+    if (this.preventPM(message)) return;
+
+    var result = [];
+    for (key in this.player) {
+        var employee = new Employee(this.employeeDatabase.getEmployeeById(this.player[key].characterId));
+        employee.setExp(this.player[key].exp);
+        result.push({
+            userId: key,
+            employee: employee
+        });
+    }
+    result.sort(function(a, b) {
+        if (a.employee.levelCached != b.employee.levelCached) {
+            return b.employee.levelCached - b.employee.levelCached;    
+        } else {
+            return b.employee.getBaseRarity() - a.employee.getBaseRarity();
+        }
+    })
+    
+    message.guild.fetchMembers().then(guild => {
+        var count = 0;
+        var text = "Your Ranking:\n";
+        var userId = message.author.id;
+        var userOrder = 0;
+        for(;userOrder<result.length;userOrder++) {
+            if (result[userOrder].userId === userId) break;
+        }
+        if (userOrder == result.length) {
+            message.reply("You are not in the ranking.");
+            return;
+        } else {
+            var count = 0;
+            var text = "Your ranking:\n";
+            var lower_bound = Math.min(userOrder + 4, result.length-1);
+            var upper_bound = Math.max(lower_bound - 9, 0);
+            for(var i=0;i<result.length;i++) {
+                if (i==0 || result[i-1].point != result[i].point) count = i;
+                var member = guild.members.find('id', result[i].userId);
+                var emojiName = 'k' + result[i].employee.getClass().toLowerCase();
+                const classEmoji = (message.guild == null ? null : message.guild.emojis.find('name', emojiName));
+                if (member && (upper_bound <= i) && (i<=lower_bound)) {
+                    if (i === userOrder) {
+                        text += "**" + (count+1) + ". " + member.user.username + "** (**" + result[i].employee.shortName + "** " + (classEmoji == null?"":classEmoji) +", Lv.**" + result[i].employee.levelCached + "**)\n";
+                    } else {
+                        text += (count+1) + ". " + member.user.username + " (**" + result[i].employee.shortName + "** " + (classEmoji == null?"":classEmoji) +", Lv.**" + result[i].employee.levelCached + "**)\n";
+                    }
+                }
+            }
+            message.channel.sendMessage(text);
+        }
+    }).catch(err => {
+        message.channel.sendMessage("Fetching member error! " + err);
+    });
+}
+
 EmployeeBot.prototype.handleCommonCommand = function(message) {
     if (message.author.bot === true) return;
     this.handleEventCommand(message);
@@ -817,6 +875,7 @@ EmployeeBot.prototype.handleCommonCommand = function(message) {
     this.handleCharaCommand(message);
     this.handleMeCommand(message);
     this.handleTopCommand(message);
+    this.handleMyTopCommand(message);
     this.handleSpecialCase(message);
 }
 
