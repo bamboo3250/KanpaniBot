793246c2a96677873383c2dee11aced222bfd4c6
Truong Ngoc Khanh <bambooVN_3250@yahoo.com>
2016-11-18 11:27:15 +0800

    add item and quest database

---
+++ database/ItemInfoList.js
@@ -0,0 +1,167 @@
+var itemInfos = [
+    {
+        _id: "70000101_1", itemName: "Copper Ore", cost: 50
+    },{
+        _id: "70000102_1", itemName: "Iron Ore", cost: 150
+    },{
+        _id: "70000103_1", itemName: "Silver Ore", cost: 500
+    },{
+        _id: "70000104_1", itemName: "Gold Ore", cost: 2000
+    },{
+        _id: "70000105_1", itemName: "Mithril Ore", cost: 5000
+    },{
+        _id: "70000106_1", itemName: "Demon Ore", cost: 5000
+    },{
+        _id: "70000107_1", itemName: "Spirit Ore", cost: 6000
+    },
+
+    {
+        _id: "70001101_1", itemName: "Pure White Cloth", cost: 25
+    },{
+        _id: "70001102_1", itemName: "Beautiful Cloth", cost: 75
+    },{
+        _id: "70001103_1", itemName: "Luxurious Cloth", cost: 125
+    },{
+        _id: "70001104_1", itemName: "Ominous Cloth", cost: 175
+    },{
+        _id: "70001105_1", itemName: "Holy Cloth", cost: 750
+    },{
+        _id: "70001106_1", itemName: "Demonic Cloth", cost: 750
+    },{
+        _id: "70001107_1", itemName: "Spirit Cloth", cost: 5000
+    },
+
+    {
+        _id: "70002101_1", itemName: "Mountain Goat Horn", cost: 25
+    },{
+        _id: "70002102_1", itemName: "Water Buffalo Horn", cost: 75
+    },{
+        _id: "70002103_1", itemName: "Unicorn Horn", cost: 250
+    },{
+        _id: "70002104_1", itemName: "Chimera Horn", cost: 1000
+    },{
+        _id: "70002105_1", itemName: "Dragon Horn", cost: 2500
+    },{
+        _id: "70002106_1", itemName: "Demon Beast Horn", cost: 2500
+    },{
+        _id: "70002107_1", itemName: "Spirit Beast Horn", cost: 750
+    },
+
+    {
+        _id: "70003101_1", itemName: "Crystal", cost: 3000
+    },{
+        _id: "70003102_1", itemName: "Diamond", cost: 20000
+    },{
+        _id: "70003103_1", itemName: "Ruby", cost: 3000
+    },{
+        _id: "70003104_1", itemName: "Rose Quartz", cost: 20000
+    },{
+        _id: "70003105_1", itemName: "Onyx", cost: 3000
+    },{
+        _id: "70003106_1", itemName: "Black Pearl", cost: 20000
+    },{
+        _id: "70003107_1", itemName: "Aquamarine", cost: 3000
+    },{
+        _id: "70003108_1", itemName: "Lapis Lazuli", cost: 20000
+    },{
+        _id: "70003109_1", itemName: "Topaz", cost: 3000
+    },{
+        _id: "70003110_1", itemName: "Garnet", cost: 20000
+    },{
+        _id: "70003111_1", itemName: "Turquoise", cost: 3000
+    },{
+        _id: "70003112_1", itemName: "Emerald", cost: 20000
+    },{
+        _id: "70003113_1", itemName: "Opal", cost: 3000
+    },{
+        _id: "70003114_1", itemName: "Amethyst", cost: 20000
+    },{
+        _id: "70003115_1", itemName: "Malachite", cost: 3000
+    },{
+        _id: "70003116_1", itemName: "Jade", cost: 20000
+    },{
+        _id: "70003117_1", itemName: "Tanzanite", cost: 3000
+    },{
+        _id: "70003118_1", itemName: "Sapphire", cost: 20000
+    },
+
+    {
+        _id: "70004101_1", itemName: "Soft Leather", cost: 25
+    },{
+        _id: "70004102_1", itemName: "Tanned Leather", cost: 75
+    },{
+        _id: "70004103_1", itemName: "Hard Leather", cost: 125
+    },{
+        _id: "70004104_1", itemName: "Luxurious Leather", cost: 1000
+    },{
+        _id: "70004105_1", itemName: "Rare Species Leather", cost: 750
+    },{
+        _id: "70004106_1", itemName: "Demon Beast Leather", cost: 750
+    },{
+        _id: "70004107_1", itemName: "Spirit Beast Leather", cost: 750
+    },
+
+    {
+        _id: "70005101_1", itemName: "Star Fragment", cost: 50
+    },{
+        _id: "70005102_1", itemName: "Crescent Moon Fragment", cost: 150
+    },{
+        _id: "70005103_1", itemName: "Half Moon Fragment", cost: 500
+    },{
+        _id: "70005104_1", itemName: "Full Moon Fragment", cost: 2000
+    },{
+        _id: "70005105_1", itemName: "Sun Fragment", cost: 5000
+    },{
+        _id: "70005106_1", itemName: "Demon World Fragment", cost: 5000
+    },{
+        _id: "70005107_1", itemName: "Spirit Gem Fragment", cost: 5000
+    },
+
+    {
+        _id: "70006101_1", itemName: "Water", cost: 10
+    },{
+        _id: "70006102_1", itemName: "Clear Water", cost: 30
+    },{
+        _id: "70006103_1", itemName: "Shimmering Water", cost: 100
+    },{
+        _id: "70006104_1", itemName: "Magical Water", cost: 400
+    },{
+        _id: "70006105_1", itemName: "Holy Water", cost: 1000
+    },{
+        _id: "70006106_1", itemName: "Beautiful Demonic Water", cost: 1000
+    },{
+        _id: "70006107_1", itemName: "Shining Spirit Water", cost: 750
+    },
+
+    {
+        _id: "70007101_1", itemName: "Tree Branch", cost: 50
+    },{
+        _id: "70007102_1", itemName: "Evergreen Oak Branch", cost: 150
+    },{
+        _id: "70007103_1", itemName: "Maple Branch", cost: 500
+    },{
+        _id: "70007104_1", itemName: "Ebony Branch", cost: 2000
+    },{
+        _id: "70007105_1", itemName: "Yggdrasil Branch", cost: 5000
+    },{
+        _id: "70007106_1", itemName: "Demon Tree Branch", cost: 5000
+    },{
+        _id: "70007107_1", itemName: "Spirit Tree Branch", cost: 2000
+    },
+
+    {
+        _id: "70020101_1", itemName: "Old Silver Coin", cost: 500
+    },{
+        _id: "70020102_1", itemName: "Rare Silver Coin", cost: 2500
+    },{
+        _id: "70020103_1", itemName: "Silver Coin Purse", cost: 700
+    },{
+        _id: "70020104_1", itemName: "Magnificent Silver Coin", cost: 3000
+    },
+
+    {
+        _id: "80000101_1", itemName: "Labyrinth Fragment", cost: 50
+    }
+];
+
+module.exports = itemInfos;
