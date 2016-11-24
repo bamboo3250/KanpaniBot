e2a003f73a8bb2346c6554f5791b4ad84c2a7e71
Truong Ngoc Khanh <bambooVN_3250@yahoo.com>
2016-11-25 04:33:12 +0800

    update ~givebread command

--- commands/GiveBreadCommand.js
+++ commands/GiveBreadCommand.js
@@ -40,21 +40,33 @@ module.exports = {
             }
             return;
         }
-
-        if (bot.breadReceived[receiverId] + amount > 5) {
-            if (bot.breadReceived[receiverId] >= 5) {
-                message.reply("The receiver can't receive any bread more today.");
-            } else {
-                message.reply("The receiver can only receive " + (5 - bot.breadReceived[receiverId]) + " bread more today.");
+        console.log(receiverId);
+        var guild = message.guild;
+        if (guild) {
+            var receiver = guild.members.find('id', receiverId);
+            console.log(receiver.user);
+            if (!receiver || receiver.user.presence.status === "offline") {
+                message.reply("You can't send bread to that member.");
+                return;
             }
-            return;
+            if (bot.breadReceived[receiverId] + amount > 5) {
+                if (bot.breadReceived[receiverId] >= 5) {
+                    message.reply("The receiver can't receive any bread more today.");
+                } else {
+                    message.reply("The receiver can only receive " + (5 - bot.breadReceived[receiverId]) + " bread more today.");
+                }
+                return;
+            }
+            if (bot.consumeBread(message, amount)) {
+                bot.initBreadIfNeed(receiverId);
+                bot.remainingBread[receiverId] += amount;
+                bot.breadReceived[receiverId] += amount;
+                message.reply(amount + " Bread has been transfered.");
+            }
+        } else {
+            message.reply("You can use this command here!");
         }
 
-        if (bot.consumeBread(message, amount)) {
-            bot.initBreadIfNeed(receiverId);
-            bot.remainingBread[receiverId] += amount;
-            bot.breadReceived[receiverId] += amount;
-            message.reply(amount + " Bread has been transfered.");
-        }
+        
     }
 }
