c8795bc9f6f9535c8d00940fbf2d4b95073c06df
Truong Ngoc Khanh <bambooVN_3250@yahoo.com>
2016-11-29 10:09:33 +0700

    check tier

--- commands/CraftCommand.js
+++ commands/CraftCommand.js
@@ -83,7 +83,9 @@ module.exports = {
         }
 
         var distribution = [20, 35, 35, 10];    // tier 1
-        //var distribution = [50, 39, 10, 1];
+        if (weaponResult.tier == 2) {
+            distribution = [50, 39, 10, 1];
+        }
         var plus = bot.functionHelper.randomDist(distribution);
 
         var weaponUrl = bot.urlHelper.getEquipmentIconUrl(weaponResult._id, plus);
