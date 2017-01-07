ea76869d519a230d9613fda9d93f9359a176333e
Truong Ngoc Khanh <bambooVN_3250@yahoo.com>
2017-01-07 21:45:29 +0700

    fix exp rate of healing

--- controllers/TrainingController.js
+++ controllers/TrainingController.js
@@ -278,7 +278,7 @@ TrainingController.prototype.attackRecursively = function(skill, attacker, targe
             text += "\thealing **";
             for(var i=0;i<damageList[targetId].length;i++) {
                 var healHp = damageList[targetId][i].damage;
-                expGained[attacker.playerId] += healHp * 10;
+                expGained[attacker.playerId] += healHp * 4;
 
                 if (i === damageList[targetId].length - 1) {
                     text += healHp + "";    
