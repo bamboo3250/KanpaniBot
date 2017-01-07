84cdeb68175ccfd4769b7b8c6742479abe6ecb5b
Truong Ngoc Khanh <bambooVN_3250@yahoo.com>
2017-01-07 22:59:30 +0700

    fix

--- controllers/TrainingController.js
+++ controllers/TrainingController.js
@@ -172,7 +172,7 @@ TrainingController.prototype.attackRecursively = function(skill, attacker, targe
                 var atk = attacker.getAtk();
                 var skillModifier = skillPhase.modifier;
                 var randomFactor = this.bot.functionHelper.randomArbitrary(1/1.1, 1.1);
-                var isCrit = (this.bot.functionHelper.randomInt(attacker.getCrit() + targetUnit.getLUK() + 20) < attacker.getCrit() + 10);
+                var isCrit = (this.bot.functionHelper.randomInt(attacker.getCrit() + targetUnit.getLUK() + 100) < attacker.getCrit() + 50);
                 var elementAdvantage = skillPhase.getElementFactor(targetUnit.element);
                 var def = targetUnit.getDef();
 
@@ -181,7 +181,7 @@ TrainingController.prototype.attackRecursively = function(skill, attacker, targe
                 rawDamage *= (hasSomeoneInFront ? 0.7 : 1.0);
 
 
-                var doesHit = (this.bot.functionHelper.randomInt(attacker.getHit() + targetUnit.getEva() + 20) < attacker.getHit() + 10);
+                var doesHit = (this.bot.functionHelper.randomInt(attacker.getHit() + targetUnit.getEva() + 100) < attacker.getHit() + 50);
                 if (skillPhase.isSpellAttack()) doesHit = true;
                 if (!doesHit) rawDamage = 0;
 
