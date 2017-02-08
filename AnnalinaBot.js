3f341c263e8a48bf2397d64c9bfbe6e2a641ce74
Truong Ngoc Khanh <bambooVN_3250@yahoo.com>
2017-02-09 00:32:37 +0800

    fix

--- AnnalinaBot.js
+++ AnnalinaBot.js
@@ -19,32 +19,6 @@ function getCommand(message) {
     return text.split(" ")[0];
 }
 
-myBot.bot.on("message", function(message) {
-    if (message.channel.type === "text" && message.channel.name === myBot.nutakuChannelName 
-            && message.author.id != myBot.bot.user.id) {
-        myBot.hasNewMessage = true;
-    }
-    myBot.initBreadIfNeed(message.author.id);
-
-    var command = getCommand(message);
-    switch (command) {
-    case "~question":
-        handleQuestion(message);
-        break;
-    case "~destroy":
-        console.log("destroying");
-        myBot.bot.destroy().then(function() {
-            myBot.bot = new Discord.Client();
-            myBot.login();
-            console.log("relogin");
-        });
-        break;
-    default:
-        myBot.handleCommonCommand(message);
-        break;
-    }
-});
-
 myBot.greetings = dialog.annalina.greetings;
 myBot.idleTalks = dialog.annalina.idleTalks;
 myBot.commonGoodMorning = myBot.commonGoodMorning.concat(dialog.annalina.commonGoodMorning);
@@ -160,31 +134,54 @@ if (isLocal) {
     ];
 }
 
+myBot.individualSetup = function() {
+    var that = this;
+    that.setup();
 
-myBot.bot.on("ready", function() {
-    if (myBot.ready()) {
-        for(var i=0;i<myBot.playerData.length;i++) {
-            myBot.playerManager.createUnitForPlayer(myBot.playerData[i]);    
+    that.bot.on("message", function(message) {
+        if (message.channel.type === "text" && message.channel.name === myBot.nutakuChannelName 
+                && message.author.id != myBot.bot.user.id) {
+            that.hasNewMessage = true;
         }
-        trainingController.bot = myBot;
-        trainingController.loadContribution();
-        
-        if (isLocal) {
-            trainingController.trainerField = [
-                [null, null, null],
-                [null, "240097185436270593", null]
-            ];
-        } else {
-            trainingController.trainerField = [
-                [null, "272258315441143810", "268576286060838914"],
-                [null, "272259125256388610", null]
-            ];    
-        }
-        
-        myBot.battleController = trainingController;
-        trainingController.setTimer();
-    }
-});
+        that.initBreadIfNeed(message.author.id);
 
+        var command = getCommand(message);
+        switch (command) {
+        case "~question":
+            handleQuestion(message);
+            break;
+        default:
+            that.handleCommonCommand(message);
+            break;
+        }
+    });
+
+    that.bot.on("ready", function() {
+        if (that.ready()) {
+            for(var i=0;i<that.playerData.length;i++) {
+                that.playerManager.createUnitForPlayer(that.playerData[i]);    
+            }
+            trainingController.bot = that;
+            trainingController.loadContribution();
+            
+            if (isLocal) {
+                trainingController.trainerField = [
+                    [null, null, null],
+                    [null, "240097185436270593", null]
+                ];
+            } else {
+                trainingController.trainerField = [
+                    [null, "272258315441143810", "268576286060838914"],
+                    [null, "272259125256388610", null]
+                ];    
+            }
+            
+            that.battleController = trainingController;
+            trainingController.setTimer();
+        }
+    });
+}
+
+myBot.individualSetup();
 myBot.token = config.annalina;
 myBot.login();
