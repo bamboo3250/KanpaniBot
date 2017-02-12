a1b3e625b2592e687d35ce90e94967fd18bbc9e5
Truong Ngoc Khanh <bambooVN_3250@yahoo.com>
2017-02-12 19:06:53 +0800

    new BF

--- RukaBot.js
+++ RukaBot.js
@@ -77,13 +77,13 @@ if (isLocal) {
 } else {
     myBot.playerData = [
         {
-            _id: "241511566036434945",  // Elsa Guest
-            characterId: "10150002_765306d2",
-            exp: 5370000,//2646190,
+            _id: "272257876393721867",  // Fanaril Guest
+            characterId: "10150003_e989854c",
+            exp: 5370000,//10707880,
             gold: 0,
             equipedWeapon: {
-                _id: "308119",
-                plus: 3
+                _id: "3001980",
+                plus: 4
             },
             equipedArmor: {
                 _id: myBot.randomArmor(1),
@@ -101,13 +101,13 @@ if (isLocal) {
             partnerId: null,
             isTrainer: true
         },{
-            _id: "278911842859089920",  // Nhano Guest
-            characterId: "10550002_5cc7900c",
+            _id: "270767219875643392",  // Siegrid Guest
+            characterId: "10550001_27d912ef",
             exp: 5370000,//10707880,
             gold: 0,
             equipedWeapon: {
-                _id: "308524",
-                plus: 3
+                _id: "3005980",
+                plus: 4
             },
             equipedArmor: {
                 _id: myBot.randomArmor(5),
@@ -125,20 +125,20 @@ if (isLocal) {
             partnerId: null,
             isTrainer: true
         },{
-            _id: "272258315441143810",  // Elmina Guest
-            characterId: "10450002_bbda369b",
+            _id: "268576286060838914",  // Saimi Guest
+            characterId: "10750001_32935980",
             exp: 5370000,//10707880,
             gold: 0,
             equipedWeapon: {
-                _id: "308419",
-                plus: 3
+                _id: "3007980",
+                plus: 4
             },
             equipedArmor: {
-                _id: myBot.randomArmor(4),
+                _id: myBot.randomArmor(7),
                 plus: 3
             },
             equipedAccessory: {
-                _id: "330107",
+                _id: "330207",
                 plus: 3
             },
             materialList: {},
@@ -154,8 +154,8 @@ if (isLocal) {
             exp: 5370000,//10707880,
             gold: 0,
             equipedWeapon: {
-                _id: "308320",
-                plus: 3
+                _id: "3003980",
+                plus: 4
             },
             equipedArmor: {
                 _id: myBot.randomArmor(3),
@@ -178,16 +178,16 @@ if (isLocal) {
             exp: 5370000,//10707880,
             gold: 0,
             equipedWeapon: {
-                _id: "308806",
-                plus: 3
+                _id: "3008980",
+                plus: 4
             },
             equipedArmor: {
                 _id: myBot.randomArmor(8),
                 plus: 3
             },
             equipedAccessory: {
-                _id: "329997",
-                plus: 4
+                _id: "330107",
+                plus: 3
             },
             materialList: {},
             weaponList: {},
@@ -214,7 +214,7 @@ myBot.bot.on("ready", function() {
             ];
         } else {
             trainingController.trainerField = [
-                ["278911842859089920", "241511566036434945", "272258315441143810"],
+                ["270767219875643392", "272257876393721867", "268576286060838914"],
                 ["269733140635975680", null, "239141420194070530"]
             ];    
         }
