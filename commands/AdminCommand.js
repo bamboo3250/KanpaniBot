a1a67b85c542640600a61568957c32008006b686
Truong Ngoc Khanh <bambooVN_3250@yahoo.com>
2017-02-17 09:06:15 +0800

    add silence & unsilence command

--- commands/AdminCommand.js
+++ commands/AdminCommand.js
@@ -18,5 +18,21 @@ module.exports = {
             bot.battleController.resetAllTrainers();
             return;
         }
+        if (command.commandName === "~unsilence") {
+            var userId = command.args[0];
+            if (!userId) return;
+            bot.userManager.removeRole(userId, "Reported");
+            bot.silenced[userId] = false;
+            bot.saveSilenced();
+            return;
+        }
+        if (command.commandName === "~silence") {
+            var userId = command.args[0];
+            if (!userId) return;
+            bot.userManager.addRole(userId, "Reported");
+            bot.silenced[userId] = true;
+            bot.saveSilenced();
+            return;
+        }
     }
 }
