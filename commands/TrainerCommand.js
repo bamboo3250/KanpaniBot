1a367219d9934e3e61acd921a05110cd187dc02d
Truong Ngoc Khanh <bambooVN_3250@yahoo.com>
2017-03-01 14:12:29 +0800

    add promotion text in trainer command

--- commands/TrainerCommand.js
+++ commands/TrainerCommand.js
@@ -24,7 +24,10 @@ module.exports = {
                     //const elementEmoji = (message.guild == null ? trainerUnit.element : message.guild.emojis.find('name', 'k' + trainerUnit.element));
                     const classEmoji = (message.guild == null ? trainerUnit.getClass() : message.guild.emojis.find('name', 'k' + trainerUnit.getClass().toLowerCase()));
                     text += "User: **" + trainerUser.username + "**\n";
-                    text += "Character: **" + trainerUnit.fullName + "** (" + (classEmoji?classEmoji+", ":"") + "Lv.**" + trainerUnit.levelCached  + "**)\n";
+                    
+                    var promotionText = (trainerUnit.promotion>1?"**Section Manager**, ":(trainerUnit.promotion>0?"**Chief**, ":""));
+                    var levelText = "Lv.**" + trainerUnit.levelCached  + "**";
+                    text += "Character: **" + trainerUnit.fullName + "(" + (classEmoji?classEmoji+", ":"")  + promotionText + levelText + ")\n";
                     var now = new Date();
                     var percentHP = Math.floor(trainerUnit.getCurrentHP()/trainerUnit.getMaxHP()*100);
                     text += "HP: **" + trainerUnit.getCurrentHP() + "/" + trainerUnit.getMaxHP() + " (" + percentHP +"%)**" + (trainerUnit.respawnTime?" (Respawn in " + bot.functionHelper.parseTime(trainerUnit.respawnTime - now.valueOf()) + ")":"") + "\n";
