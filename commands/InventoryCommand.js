fc775fed088eddf3c26f40816f4a843457ad6e15
Truong Ngoc Khanh <bambooVN_3250@yahoo.com>
2017-03-19 00:05:22 +0800

    remove image in ~inventory command

--- commands/InventoryCommand.js
+++ commands/InventoryCommand.js
@@ -45,52 +45,53 @@ module.exports = {
         }
 
         var text = "Your Inventory:\n";
+        message.author.sendMessage(text + backupItemDropText);
 
-        bot.imageHelper.download(queue, function(err) {
-            if (err) {
-                message.author.sendMessage(text + backupItemDropText);
-                return;
-            }
-            bot.imageHelper.read(itemFileNameList, function (err, imageDict) {
-                if (err) {
-                    message.author.sendMessage(text + backupItemDropText);
-                    return;
-                }
+        // bot.imageHelper.download(queue, function(err) {
+        //     if (err) {
+        //         message.author.sendMessage(text + backupItemDropText);
+        //         return;
+        //     }
+        //     bot.imageHelper.read(itemFileNameList, function (err, imageDict) {
+        //         if (err) {
+        //             message.author.sendMessage(text + backupItemDropText);
+        //             return;
+        //         }
 
-                const ITEM_CELL_WIDTH = 240;
-                const ITEM_CELL_HEIGHT = 50;
-                const NUM_COL = 3;
+        //         const ITEM_CELL_WIDTH = 240;
+        //         const ITEM_CELL_HEIGHT = 50;
+        //         const NUM_COL = 3;
 
-                var imageList = [];
-                for(key in imageDict) imageList.push(imageDict[key]);
+        //         var imageList = [];
+        //         for(key in imageDict) imageList.push(imageDict[key]);
 
-                var imageWidth = ITEM_CELL_WIDTH*NUM_COL;
-                var imageHeight = Math.max(1, Math.ceil(imageList.length/NUM_COL)) * ITEM_CELL_HEIGHT;
+        //         var imageWidth = ITEM_CELL_WIDTH*NUM_COL;
+        //         var imageHeight = Math.max(1, Math.ceil(imageList.length/NUM_COL)) * ITEM_CELL_HEIGHT;
 
-                var image = new Jimp(imageWidth, imageHeight, 0xFFFFFFFF, function (err, image) {
-                    if (err) {
-                        message.author.sendMessage(text + backupItemDropText);
-                        return;
-                    }
+        //         var image = new Jimp(imageWidth, imageHeight, 0xFFFFFFFF, function (err, image) {
+        //             if (err) {
+        //                 message.author.sendMessage(text + backupItemDropText);
+        //                 return;
+        //             }
 
-                    Jimp.loadFont(Jimp.FONT_SANS_16_BLACK).then(function (font) {
-                        image.opacity(0.4);
+        //             Jimp.loadFont(Jimp.FONT_SANS_16_BLACK).then(function (font) {
+        //                 image.opacity(0.4);
 
-                        for(var i=0;i<imageList.length;i++) {
-                            var row = Math.floor(i/NUM_COL);
-                            var col = i%NUM_COL;
-                            image.composite(imageList[i], 7 + col*ITEM_CELL_WIDTH, 7 + row*ITEM_CELL_HEIGHT);
-                            image.print(font, 55 + col*ITEM_CELL_WIDTH, 7 + row*ITEM_CELL_HEIGHT, itemInfoList[i].itemName);
-                            image.print(font, 55 + col*ITEM_CELL_WIDTH, 27 + row*ITEM_CELL_HEIGHT, "x" + player.materialList[itemInfoList[i].itemName]);
-                            image.print(font, 125 + col*ITEM_CELL_WIDTH, 27 + row*ITEM_CELL_HEIGHT, "Price: " + itemInfoList[i].price);
-                        }
-                        var imageName = "images/inventory/" + userId + ".png";
-                        image.write(imageName, function() {
-                            message.author.sendFile(imageName, "png", text);
-                        });
-                    });
-                });
-            });
-        });
+        //                 for(var i=0;i<imageList.length;i++) {
+        //                     var row = Math.floor(i/NUM_COL);
+        //                     var col = i%NUM_COL;
+        //                     image.composite(imageList[i], 7 + col*ITEM_CELL_WIDTH, 7 + row*ITEM_CELL_HEIGHT);
+        //                     image.print(font, 55 + col*ITEM_CELL_WIDTH, 7 + row*ITEM_CELL_HEIGHT, itemInfoList[i].itemName);
+        //                     image.print(font, 55 + col*ITEM_CELL_WIDTH, 27 + row*ITEM_CELL_HEIGHT, "x" + player.materialList[itemInfoList[i].itemName]);
+        //                     image.print(font, 125 + col*ITEM_CELL_WIDTH, 27 + row*ITEM_CELL_HEIGHT, "Price: " + itemInfoList[i].price);
+        //                 }
+        //                 var imageName = "images/inventory/" + userId + ".png";
+        //                 image.write(imageName, function() {
+        //                     message.author.sendFile(imageName, "png", text);
+        //                 });
+        //             });
+        //         });
+        //     });
+        // });
     }
 }
