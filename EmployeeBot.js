9e79ba93cbc76aa45a0a7b6b046bbf4cddc9ac40
Truong Ngoc Khanh <bambooVN_3250@yahoo.com>
2017-06-20 11:05:09 +0700

    update schedule

--- EmployeeBot.js
+++ EmployeeBot.js
@@ -112,16 +112,16 @@ function EmployeeBot() {
     this.schedule = [
         {
             name        : "Kanpani☆Girls Maintenance",
-            startTime   : "June 16 2017 13:00:00 GMT+0900",
-            endTime     : "June 16 2017 17:00:00 GMT+0900"
+            startTime   : "June 23 2017 14:00:00 GMT+0900",
+            endTime     : "June 23 2017 17:00:00 GMT+0900"
         },{
             name        : "Kanpani☆Storytelling Campaign",
             startTime   : "Mar 17 2017 17:00:00 GMT+0900",
             endTime     : "Mar 24 2017 14:00:00 GMT+0900"
         },{
             name        : "Presidents!_It's_Level_Up_Time_Again! Campaign",
-            startTime   : "Mar 24 2017 17:00:00 GMT+0900",
-            endTime     : "Mar 31 2017 14:00:00 GMT+0900"
+            startTime   : "June 16 2017 17:00:00 GMT+0900",
+            endTime     : "June 23 2017 14:00:00 GMT+0900"
         },{
             name        : "Kanpani☆June_Illness_and_Demon's_Newcomer_Training Event",
             startTime   : "May 31 2017 17:00:00 GMT+0900",
